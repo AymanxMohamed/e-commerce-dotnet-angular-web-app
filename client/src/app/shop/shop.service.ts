7681ca7b83b20bad4ac4528db95b44e4bc5347fc
Ayman Kheireldeen <aymanmido067@gmail.com>
2023-02-28 02:10:40 +0200

    Finishing the shop component with paging functionality

---
+++ client/src/app/shop/shop.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Brand } from '../shared/models/brand';
+import { Pagination } from '../shared/models/pagination';
+import { Product } from '../shared/models/product';
+import { ShopParams } from '../shared/models/shopParams';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ShopService {
+  baseUrl = 'https://localhost:5001/api/products';
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(shopParams: ShopParams) {
+    let { brandId, typeId, sort, pageNumber, pageSize, search } = shopParams;
+    let params = new HttpParams();
+
+    if (brandId > 0) params = params.append('brandId', brandId);
+    if (typeId) params = params.append('typeId', typeId);
+    params = params.append('sort', sort);
+    params = params.append('pageIndex', pageNumber);
+    params = params.append('pageSize', pageSize);
+    if (search) params = params.append('search', search);
+    return this.http.get<Pagination<Product[]>>(`${this.baseUrl}`, {
+      params,
+    });
+  }
+
+  getBrands() {
+    return this.http.get<Brand[]>(`${this.baseUrl}/brands`);
+  }
+
+  getTypes() {
+    return this.http.get<Brand[]>(`${this.baseUrl}/types`);
+  }
+}
